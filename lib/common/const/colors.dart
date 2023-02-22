98b32f2dcabb5df12fb4a3dd6768156de4d71105
ZanacXevious <ckongmario21@outlook.com>
2023-02-22 12:04:15 -0600

    1_1 색상 상수 지정하기 _1

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,13 @@
+import "dart:ui";
+
+// import material package
+import 'package:flutter/material.dart';
+
+// 주색상
+const PRIMARY_COLOR = Color(0xFF4CAF50);
+// 글자 색상
+const BODY_TEXT_COLOR = Color(0xFF9E9E9E);
+// 텍스트 필드 배경색
+const INPUT_BG_COLOR = Color(0xFFF5F5F5);
+// 텍스트 필드 테두리 색
+const INPUT_BORDER_COLOR = Color(0xFFE0E0E0);
