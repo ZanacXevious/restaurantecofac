8f60ad4c87b8abc2aba104146ff128e50d5134b1
ZanacXevious <ckongmario21@outlook.com>
2023-02-22 15:45:21 -0600

    1_2 텍스트필드 디자인하기

---
+++ lib/common/component/custom_test_form_field.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantecofac/common/const/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.hintText,
+    this.errorText,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var baseBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        // hintText의 색상을 BODY_TEXT_COLOR로 설정하고 fontSize를 14로 설정한다
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14,
+        ),
+        // Background Color of TextFormField Container set to BODY_TEXT_COLOR
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        // input border를 테두리로 두르고 색상을 INPUT_BORDER_COLOR로 설정한다
+        border: baseBorder,
+        // TextFormField를 선택했을 때 테두리 색상을 PRIMARY_COLOR로 설정한다
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurantecofac/common/component/custom_test_form_field.dart';
 
 void main() {
   runApp(_App());
@@ -12,11 +13,21 @@ class _App extends StatelessWidget {
       // remove debug banner
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is my default text!'),
-      ),
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomTextFormField(
+            hintText: "이메일을 입력해 주세요",
+            onChanged: (String value) {},
+          ),
+          // password field
+          CustomTextFormField(
+            hintText: "비밀번호를 입력해 주세요",
+            obscureText: true,
+            onChanged: (String value) {},
+          ),
+        ],
+      )),
     );
   }
 }
